06ccba77631f9c5426bbbf987ce3729358121863
Shubham Patil <shubhampatil871@gmail.com>
2024-04-30 18:55:30 +0530

    add useParams

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout.jsx";
 import Home from "./components/Home/Home.jsx";
 import About from "./components/About/About.jsx";
 import Contact from "./components/Contact/Contact.jsx";
+import User from "./components/Users/User.jsx";
 
 // const router = createBrowserRouter([
 //   {
@@ -31,15 +37,14 @@ import Contact from "./components/Contact/Contact.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout/>}>
-      <Route path="" element={<Home/>}></Route>
-      <Route path="/about" element={<About/>}></Route>
-      <Route path="/contact" element={<Contact/>}></Route>
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
     </Route>
   )
-)
-
-
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
