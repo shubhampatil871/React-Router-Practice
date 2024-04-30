3d7c36f6eb95c2e34458993159525de4dcf2998f
Shubham Patil <shubhampatil871@gmail.com>
2024-04-30 18:55:10 +0530

    add user component

---
+++ src/components/Users/User.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import { useParams } from "react-router-dom";
+
+function User() {
+  const { userid } = useParams();
+
+  return <div>User:{userid}</div>;
+}
+
+export default User;
